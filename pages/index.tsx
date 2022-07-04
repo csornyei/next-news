625dbdff2ed3100c6c10bf88e1a47dd184417336
Mate Mark Csornyei <mate@csornyei.com>
2022-07-04 22:35:18 +0200

    Cors proxy added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
+import axios from "axios";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const buttonClicked = async () => {
+    const { data } = await axios.get(
+      `/api/cors?url=https://www.esquire.com/rss/style.xml/`
+    );
+    console.log(data);
+  };
+
   return (
     <div>
-      <button className="btn btn-primary w-64 rounded">Get Feeds</button>
+      <button className="btn btn-primary w-64 rounded" onClick={buttonClicked}>
+        Get Feeds
+      </button>
     </div>
   );
 };
