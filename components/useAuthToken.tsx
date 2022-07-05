f249b2bb9999bac7683f292b365f09c4c57dd687
Mate Mark Csornyei <mate@csornyei.com>
2022-07-05 22:09:51 +0200

    useInput in useAuthToken

--- components/useAuthToken.tsx
+++ components/useAuthToken.tsx
@@ -1,26 +1,21 @@
 import { useState } from "react";
+import useInput from "./useInput";
 
 function useAuthToken() {
-  const [inputText, setInputText] = useState("");
+  const { value: inputText, component: Input } = useInput();
   const [authToken, setAuthToken] = useState("");
   const AuthTokenForm = (
     <div className="form-control ml-6 mt-4 mr-6 md:mr-0">
       <label className="label">
         <span className="label-text">Auth token</span>
       </label>
-      <input
-        type="text"
-        placeholder="Auth token"
-        className="input input-bordered w-full max-w-xs"
-        onChange={({ target: { value } }) => {
-          setInputText(value);
-        }}
-      />
+      {Input()}
       <button
         className="btn btn-sm btn-primary w-16 mt-2"
         onClick={() => {
           setAuthToken(inputText);
         }}
+        disabled={inputText.length === 0}
       >
         Set
       </button>
