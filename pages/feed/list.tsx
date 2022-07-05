69ca239d2fd0f3d410604b8c84aad5377ba8f846
Mate Mark Csornyei <mate@csornyei.com>
2022-07-05 20:49:17 +0200

    List refactor

--- pages/feed/list.tsx
+++ pages/feed/list.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import axios from "axios";
+import useAuthToken from "../../components/useAuthToken";
+import Loader from "../../components/icons/Loader";
+import Delete from "../../components/icons/Delete";
 import { LocalFeed } from "../../utils/types";
 import { connectToDatabase, getFeeds } from "../../utils/database";
 
@@ -6,8 +11,25 @@ interface FeedListPageProps {
 }
 
 function FeedListPage({ feeds }: FeedListPageProps) {
+  const { authToken, AuthTokenForm } = useAuthToken();
+  const [loadings, setLoadings] = useState<string[]>([]);
+
+  const addLoading = (id: string) => {
+    setLoadings([...loadings, id]);
+  };
+
+  const isLoading = (id: string) => {
+    return loadings.indexOf(id) !== -1;
+  };
+
+  const removeLoading = (id: string) => {
+    const oldLoadings = [...loadings];
+    setLoadings(oldLoadings.filter((i) => i !== id));
+  };
+
   return (
     <div>
+      {authToken}
       <table className="table mx-6 mt-4">
         <thead>
           <tr>
@@ -33,29 +55,37 @@ function FeedListPage({ feeds }: FeedListPageProps) {
                   ))}
                 </td>
                 <td>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 cursor-pointer text-error"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    onClick={() => {
-                      console.log(`Deleting ${feed.id}`);
-                    }}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                  {isLoading(feed.id) ? (
+                    <Loader />
+                  ) : (
+                    <Delete
+                      onClick={async () => {
+                        addLoading(feed.id);
+                        try {
+                          const res = await axios.delete(
+                            `/api/delete-feed?id=${feed.id}`,
+                            {
+                              headers: {
+                                Authorization: authToken,
+                              },
+                            }
+                          );
+                          console.log(res);
+                        } catch (error) {
+                          console.error(error);
+                        } finally {
+                          removeLoading(feed.id);
+                        }
+                      }}
                     />
-                  </svg>
+                  )}
                 </td>
               </tr>
             );
           })}
         </tbody>
       </table>
+      {AuthTokenForm}
     </div>
   );
 }
